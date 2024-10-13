aa1c226e14161a6d46f42168230894c2d2f7085a
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2024-10-13 12:38:20 +0530

    Create crDroid.mk

---
+++ crDroid.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+# OTA
+# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.system.ota.json_url=https://raw.githubusercontent.com/rajansingh9745/crDroid_gsi/14/ota.json
+
+# Set Bootanimation at 1080P
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# APN
+PRODUCT_PACKAGES += apns-conf.xml
