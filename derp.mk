08efda15089e4ee10d3896f75c54765af9a22d0f
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2024-10-13 12:36:24 +0530

    Create derp.mk

---
+++ derp.mk
@@ -0,0 +1,21 @@
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common.mk)
+$(call inherit-product, vendor/derp/config/BoardConfigDerpFest.mk)
+$(call inherit-product, vendor/derp/config/BoardConfigSoong.mk)
+$(call inherit-product, device/derp/sepolicy/common/sepolicy.mk)
+-include vendor/derp/build/core/config.mk
+                           
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_NOT_USES_BLUR := true
+EXTRA_UDFPS_ANIMATIONS := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/KoysX/treble_DerpFest_GSI/14/ota.json
