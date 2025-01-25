9fcf2ad6f23201a1610f21474fde2cec16a1704b
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2025-01-25 13:30:09 +0530

    Update othertree.sh

--- othertree.sh
+++ othertree.sh
@@ -2,19 +2,19 @@
 
 # Clone each repository
 echo "Cloning TrebleDroid/vendor_hardware_overlay..."
-git clone https://github.com/rajansingh9745/vendor_hardware_overlay.git -b pie vendor/hardware_overlay
+git clone https://github.com/rajansingh9745/vendor_hardware_overlay -b pie vendor/hardware_overlay
 
 echo "Cloning TrebleDroid/vendor_interfaces..."
-git clone https://github.com/rajansingh9745/vendor_interfaces.git -b android-15.0 vendor/interfaces
+git clone https://github.com/rajansingh9745/vendor_interfaces -b android-15.0 vendor/interfaces
 
 echo "Cloning TrebleDroid/treble_app..."
 git clone https://github.com/rajansingh9745/treble_app -b master treble_app
 
 echo "Cloning phhusson/vendor_magisk..."
-git clone https://github.com/rajansingh9745/vendor_magisk.git -b android-10.0 vendor/magisk
+git clone https://github.com/rajansingh9745/vendor_magisk -b android-10.0 vendor/magisk
 
 echo "Cloning AndyCGYan/android_packages_apps_QcRilAm..."
-git clone https://github.com/AndyCGYan/android_packages_apps_QcRilAm -b master packages/apps/QcRilAm
+git clone https://github.com/rajansingh9745/android_packages_apps_QcRilAm -b master packages/apps/QcRilAm
 
 echo "Cloning platform/prebuilts/vndk/v28..."
 git clone https://github.com/rajansingh9745/prebuilts_vndk_v28-modified --depth=1 prebuilts/vndk/v28
