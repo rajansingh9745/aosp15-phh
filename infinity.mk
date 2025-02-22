27420f9f8b0788eafc22f28e159453b56358d1ca
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2025-02-22 18:38:04 +0530

    Update infinity.mk

--- infinity.mk
+++ infinity.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, vendor/infinity/config/common.mk)
 $(call inherit-product, vendor/infinity/config/common_mobile_full.mk)
 $(call inherit-product, vendor/infinity/config/BoardConfigSoong.mk)
-$(call inherit-product, vendor/infinity/config/BoardConfigLineage.mk)
+$(call inherit-product, vendor/infinity/config/BoardConfigInfinity.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/infinity/build/core/config.mk
 TARGET_NO_KERNEL_OVERRIDE := true
