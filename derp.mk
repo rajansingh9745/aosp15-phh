10cf0cabb4443213b4c63256215659c9222bf8af
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2025-02-23 10:51:52 +0530

    Update derp.mk

--- derp.mk
+++ derp.mk
@@ -14,8 +14,8 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_NOT_USES_BLUR := true
 EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORT_BLUR := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/rajansingh9745/treble_DerpFest_GSI/14/ota.json
