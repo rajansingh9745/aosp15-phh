07a846140fb0c0c548d60ae501189fa5be48771d
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2025-06-16 13:28:38 +0530

    Update treble_arm64_bgN.mk

--- treble_arm64_bgN.mk
+++ treble_arm64_bgN.mk
@@ -2,7 +2,7 @@ TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
 
-$(call inherit-product, device/phh/treble/derp.mk)
+$(call inherit-product, device/phh/treble/evo.mk)
 
 PRODUCT_NAME := treble_arm64_bgN
 PRODUCT_DEVICE := tdgsi_arm64_ab
@@ -12,6 +12,7 @@ PRODUCT_MODEL := TrebleDroid with GApps
 
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
+WITH_GMS := true
 
 PRODUCT_PACKAGES += 
 
