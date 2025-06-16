06ee4b6baffac48d095853e0bf9a9fadfe2fd6a5
itsRajan <38017204+rajansingh9745@users.noreply.github.com>
2025-06-16 13:27:47 +0530

    Update evo.mk

--- evo.mk
+++ evo.mk
@@ -1,37 +1,22 @@
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
 $(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
-
-override PRODUCT_MODEL := Evolution X GSI
-
-# No kernel image
+-include vendor/derp/build/core/config.mk
+                           
+SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
-#BUILD_BROKEN_DUP_RULES := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_FACE_UNLOCK := true
-override TARGET_SUPPORTS_64_BIT_APPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORT_BLUR := true
+WITH_GMS := true
 
-# OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/rajansingh9745/treble_evo/main/ota.json
-
-# SELinux
-SELINUX_IGNORE_NEVERALLOWS := true
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-
-# Evolution X
-EVO_BUILD_TYPE := Unofficial
-
-# Additional packages
-PRODUCT_PACKAGES += \
-  OpenEUICC \
-  FaceUnlockService
-
-# Evolution X maintainer overlay
-#DEVICE_PACKAGE_OVERLAYS += \
-#  $(LOCAL_PATH)/overlay-evolution \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/rajansingh9745/treble_DerpFest_GSI/14/ota.json
